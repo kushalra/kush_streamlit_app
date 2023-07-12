af30d1cfec22523fbb4ded400f2f4c46d065830b
kushalra <52394507+kushalra@users.noreply.github.com>
2023-07-12 11:59:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-fruityvise_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+fruityvise_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # streamlit.text(fruityvise_response.json())
 
 # flatten the simple to moderately semi-structured nested JSON structures to flat tables
