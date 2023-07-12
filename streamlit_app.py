5f6f83b7a24bbe97ab43c76b2efd77f59b2b83cf
kushalra <52394507+kushalra@users.noreply.github.com>
2023-07-12 17:30:01 +0530

    Update streamlit_app.py
    added new fruit choice option

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,7 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# New Fruit suggestion or add
+fruit_add = streamlit.text_input('What fruit would you like information about?','Jackfruit')
+streamlit.write('Thanks for adding the ', fruit_choice)
